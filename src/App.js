55fb3f8659f53a59d8a53953fb4ee93e14522e64
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-04 17:57:11 +0100

    Added bootstrap search field and search icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /*jshint esversion: 10 */
 
-import React from 'react';
+import React from "react";
+import { FormGroup, FormControl, InputGroup } from "react-bootstrap";
+import { AiOutlineFileSearch } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App">
-      React
+      <h2>Book Search Explorer</h2>
+      <FormGroup>
+        <InputGroup>
+          <FormControl type="text" placeholder="Search for a book" />
+          <AiOutlineFileSearch />
+        </InputGroup>
+      </FormGroup>
     </div>
   );
 }
