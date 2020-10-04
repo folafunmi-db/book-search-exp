6a26ec0ae8e1893956875309b34cba23939d94d6
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-04 19:17:43 +0100

    Added searched book title display

--- src/components/Gallery/gallery.component.jsx
+++ src/components/Gallery/gallery.component.jsx
@@ -5,6 +5,13 @@ import "./gallery.styles.css";
 
 export default class Gallery extends Component {
   render() {
-    return <div>Gallery Component</div>;
+    return (
+      <div>
+        {this.props.items.map((item) => {
+          let { title } = item.volumeInfo;
+          return <div>{title}</div>;
+        })}
+      </div>
+    );
   }
 }
