e9521b667e34393164cea08b2680c793adb3f965
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-04 18:17:14 +0100

    Added state object and added logic to update query object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 /*jshint esversion: 10 */
 
 import React from "react";
-import { FormGroup, FormControl, InputGroup } from "react-bootstrap";
-import { AiOutlineFileSearch } from "react-icons/ai";
+import Global from './components/Global.component.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h2>Book Search Explorer</h2>
-      <FormGroup>
-        <InputGroup>
-          <FormControl type="text" placeholder="Search for a book" />
-          <AiOutlineFileSearch />
-        </InputGroup>
-      </FormGroup>
+      <Global />
     </div>
   );
 }
---
+++ src/components/Global.component.jsx
@@ -0,0 +1,38 @@
+/*jshint esversion: 10 */
+
+import React, { Component } from "react";
+import { FormGroup, FormControl, InputGroup } from "react-bootstrap";
+import { AiOutlineFileSearch } from "react-icons/ai";
+
+class Global extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    };
+  }
+
+  search() {
+    console.log(this.state.query);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Book Search Explorer</h2>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for a book"
+              onChange={(event) => this.setState({ query: event.target.value })}
+            />
+            <AiOutlineFileSearch onClick={() => this.search()} />
+          </InputGroup>
+        </FormGroup>
+      </div>
+    );
+  }
+}
+
+export default Global;
